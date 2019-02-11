7f524c0f7585523954b0669a0f5ac71f742e3bb7
VitaliiKalinbet <bosssvit@gmail.com>
2019-02-11 11:42:34 +0200

    Updates

---
+++ precache-manifest.4a39ca39e9f053b4b40bfa28190dd8d2.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/task_blog/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "79ce4f9008f14656ea23",
+    "url": "/task_blog/static/css/main.bb08ead2.chunk.css"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/task_blog/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "d05893714e6d88add8cf",
+    "url": "/task_blog/static/js/1.d0589371.chunk.js"
+  },
+  {
+    "revision": "7877812edbc632a4496b",
+    "url": "/task_blog/static/js/runtime~main.7877812e.js"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/task_blog/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/task_blog/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/task_blog/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/task_blog/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/task_blog/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/task_blog/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "79ce4f9008f14656ea23",
+    "url": "/task_blog/static/js/main.79ce4f90.chunk.js"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/task_blog/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/task_blog/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/task_blog/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/task_blog/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/task_blog/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/task_blog/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/task_blog/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/task_blog/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "d05893714e6d88add8cf",
+    "url": "/task_blog/static/css/1.7c5377bd.chunk.css"
+  },
+  {
+    "revision": "d40a4b2b88ea30f715a306aae3ab29c4",
+    "url": "/task_blog/index.html"
+  }
+];
